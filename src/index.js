dfd9543b50d551cdb9ac2ac488b6d829793b60a5
F4R105 <kajubufaraji@gmail.com>
2024-09-30 10:27:49 +0300

    add all mail configurations and code

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { Resend } from 'resend'
+
+const app = new Hono()
+
+app.use(cors())
+
+app.post('/send/email', async (c) => {
+  const resend = new Resend(c.env.RESEND_API_KEY)
+  const data = await resend.emails.send(
+    {
+      from: "Acme <onboarding@resend.dev>",
+      to: "kajubufaraji@gmail.com",
+      subject: "Hello from fari's cf worker",
+      html: "<p>Test successfull</p>"
+    }
+  )
+
+  return c.json(data)
+})
+
+export default app
